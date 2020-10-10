f7f7d64e64895056256bcbefe200bea978f2a371
Ville Kopio <ville.kopio@kodan.fi>
2020-10-10 22:45:30 +0300

    Fix env vars for real.

--- bandikamppa/inc/config.php
+++ bandikamppa/inc/config.php
@@ -3,5 +3,5 @@ $config = array(
 	'available_from' => 8,
 	'available_to' => 22,
 	'max_reservations' => 10,
-	'db_connection' => 'host=' . env('DB_HOST') . ' dbname=' . env('DB_NAME') . ' user=' . env('DB_USER') . ' password=' . env('DB_PASSWORD')
+	'db_connection' => 'host=' . getenv('DB_HOST') . ' dbname=' . getenv('DB_NAME') . ' user=' . getenv('DB_USER') . ' password=' . getenv('DB_PASSWORD')
 );
